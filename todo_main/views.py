b62ef104292535ec721b52ee3f71ef11c7264367
HarshithGowdaHL09 <harshithhlgowda111@gmail.com>
2025-08-25 17:14:28 +0530

    Added undo task feature and styled buttons

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from todo.models import Task
 def home(request):
-    tasks = Task.objects.filter(is_completed=False)
+    tasks1 = Task.objects.filter(is_completed=False)
+    completed = Task.objects.filter(is_completed=True)
     context = {
-        'tasks': tasks
+        'tasks': tasks1,
+        'completed':completed
     }
+    
     return render(request, 'home.html', context)
